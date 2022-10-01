5d0240af989ea9a2e9b7813b8043bcc85b66eb3a
Evgeniy <95440463+MEvgeniy21@users.noreply.github.com>
2022-10-01 17:49:06 +0300

    start

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-06-phonebook/">
+    <BrowserRouter basename="/goit-react-hw-07-phonebook/">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <ThemeProvider theme={theme}>
