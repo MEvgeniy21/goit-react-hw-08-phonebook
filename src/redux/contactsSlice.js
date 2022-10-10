bbda6b3c35d5cf60e848c78a96b5438f25ddf994
Evgeniy <95440463+MEvgeniy21@users.noreply.github.com>
2022-10-11 00:34:25 +0300

    axios

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,18 +1,46 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://63385ecf937ea77bfdbf0d7e.mockapi.io/api';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+const axiosBaseQuery =
+  ({ baseUrl } = { baseUrl: '' }) =>
+  async ({ url, method, body, params }) => {
+    try {
+      const result = await axios({
+        url: baseUrl + url,
+        method,
+        data: body,
+        params,
+      });
+      return { data: result.data };
+    } catch (axiosError) {
+      let err = axiosError;
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      };
+    }
+  };
 
 export const contactApi = createApi({
   reducerPath: 'contactApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://63385ecf937ea77bfdbf0d7e.mockapi.io/api/',
+  baseQuery: axiosBaseQuery({
+    baseUrl: '',
   }),
   tagTypes: ['Contacts'],
   endpoints: builder => ({
     getContacts: builder.query({
-      query: () => `/contacts`,
+      query: () => ({ url: `/contacts`, method: 'GET' }),
       providesTags: ['Contacts'],
     }),
     getContactById: builder.query({
-      query: id => `/contacts/${id}`,
+      query: id => ({ url: `/contacts/${id}`, method: 'GET' }),
       providesTags: ['Contacts'],
     }),
     addContact: builder.mutation({
@@ -40,6 +68,47 @@ export const contactApi = createApi({
     }),
   }),
 });
+
+// export const contactApi = createApi({
+//   reducerPath: 'contactApi',
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: 'https://63385ecf937ea77bfdbf0d7e.mockapi.io/api/',
+//   }),
+//   tagTypes: ['Contacts'],
+//   endpoints: builder => ({
+//     getContacts: builder.query({
+//       query: () => `/contacts`,
+//       providesTags: ['Contacts'],
+//     }),
+//     getContactById: builder.query({
+//       query: id => `/contacts/${id}`,
+//       providesTags: ['Contacts'],
+//     }),
+//     addContact: builder.mutation({
+//       query: newContact => ({
+//         url: `/contacts`,
+//         method: 'POST',
+//         body: newContact,
+//       }),
+//       invalidatesTags: ['Contacts'],
+//     }),
+//     deleteContact: builder.mutation({
+//       query: id => ({
+//         url: `/contacts/${id}`,
+//         method: 'DELETE',
+//       }),
+//       invalidatesTags: ['Contacts'],
+//     }),
+//     updateContact: builder.mutation({
+//       query: patch => ({
+//         url: `/contacts/${patch.id}`,
+//         method: 'PUT',
+//         body: patch,
+//       }),
+//       invalidatesTags: ['Contacts'],
+//     }),
+//   }),
+// });
 
 export const {
   useGetContactsQuery,
